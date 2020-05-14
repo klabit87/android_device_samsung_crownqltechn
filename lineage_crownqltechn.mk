f905296dc502fcb8acf83ce71fd6a1e738f8711a
klabit87 <keith.labit@gmail.com>
2020-05-13 20:25:42 -0500

    Add ota

--- lineage_crownqltechn.mk
+++ lineage_crownqltechn.mk
@@ -12,3 +12,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	BUILD_FINGERPRINT=samsung/crownqltezh/crownqltechn:10/QP1A.190711.020/N9600ZHU4DTD1:user/release-keys \
 	PRIVATE_BUILD_DESC="crownqltezh-user 10 QP1A.190711.020 N9600ZHU4DTD1 release-keys"
 endif
+
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+	lineage.updater.uri=https://raw.githubusercontent.com/klabit87/OTA/master/crownqltechn.json
